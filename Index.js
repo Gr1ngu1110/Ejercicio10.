2120df6ee9e6a9eeb6d87b837c50fe876a777f36
Juan Esteban Novillo Wagner <118315701+Gr1ngu1110@users.noreply.github.com>
2022-11-27 21:25:32 -0300

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,21 @@
+prompt("Ingresa su nonbre");
+let nombre= ("programacion" + "facil");
+let saludo= ("Les doy la bienvenida a ");
+let frasecompleta =("saludo" + "nombre");
+console.log(frasecompleta);
+let frase = "JavaScript" + " es un lenguaje " + " esencial para crear " + " sitios web poderosos. ";
+console.log(frase)
+let numeroString1 ="10";
+let numeroString2 ="20";
+let numeroString3 ="30";
+let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
+console.log (concatenaNumeros);
+var age = prompt ("¿Cual es tu edad: ?")
+console.log(age);
+let edad;
+edad = prompt ("Introduce tu edad");
+if (edad >= 18){
+    alert("Puedes entrar, eres mayor de edad.")  
+} else {
+    alert("NO PUEDES ENTRAR AQUI, ERES MENOR DE EDAD.")
+}
